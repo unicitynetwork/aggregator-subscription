f835819f519e0002fee1260a1bf6e5ffead3ef59
Risto Alas <risto.alas@gmail.com>
2025-10-02 10:47:18 +0300

    Make BigIntegers seralised as strings in JSON for safety

--- src/main/java/com/unicity/proxy/model/ObjectMapperUtils.java
+++ src/main/java/com/unicity/proxy/model/ObjectMapperUtils.java
@@ -2,6 +2,10 @@ package com.unicity.proxy.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.math.BigInteger;
 
 public class ObjectMapperUtils {
     public static ObjectMapper createObjectMapper() {
@@ -9,6 +13,11 @@ public class ObjectMapperUtils {
         result.findAndRegisterModules(); // For Java 8 time support
         result.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
         result.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        SimpleModule m = new SimpleModule();
+        m.addSerializer(BigInteger.class, ToStringSerializer.instance);
+        result.registerModule(m);
+
         return result;
     }
 }
